7f40c2c385fbe20f2fada66f60835ce23777946c
Arrow <joutsila.joosua@gmail.com>
2021-10-13 23:50:24 +0300

    w

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'click'
+        'click>=7.1.2',
+        'requests>=2.25.1'
     ],
     entry_points='''
     [console_scripts]
