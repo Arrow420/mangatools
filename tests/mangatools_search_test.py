8ae26fa587ddbb7e797c4173f68050d3bc320050
Arrow <joutsila.joosua@gmail.com>
2024-06-12 22:50:03 +0300

    tests: fukc

--- tests/mangatools_search_test.py
+++ tests/mangatools_search_test.py
@@ -6,7 +6,7 @@ class MangaToolsSearchTests(unittest.TestCase):
     def test_search_title_solanin(self):
         title = 'solanin'
         runner = CliRunner()
-        result = runner.invoke(mangatools, ['search', title], prog_name='solanin')
+        result = runner.invoke(mangatools, ['search', title], prog_name='Mangatools')
         expected_title = 'solanin'
         self.assertEqual(result.exit_code, 0)
         self.assertEqual(result.output.split("Title: ")[1].split("\n", 1)[0].lower().strip(), expected_title)
@@ -14,14 +14,15 @@ class MangaToolsSearchTests(unittest.TestCase):
     def test_search_author_one_piece(self):
         title = 'one piece'
         runner = CliRunner()
-        result = runner.invoke(mangatools, ['search', title], prog_name='one piece')
+        result = runner.invoke(mangatools, ['search', title], prog_name='Mangatools')
         expected_author = 'oda eiichiro'
         self.assertEqual(result.exit_code, 0)
         self.assertEqual(result.output.split("Author: ")[1].split("Artist: ")[0].lower().strip(), expected_author)
     
     def test_search_title_incorrect(self):
+        title = 'jkwgwgjwyufhjksdq'
         runner = CliRunner()
-        result = runner.invoke(mangatools, ['search', 'jkwgwgjwyufhjksdq'], prog_name='incorrect title')
+        result = runner.invoke(mangatools, ['search', title], prog_name='Mangatools')
         self.assertNotEqual(result.exit_code, 0)
 
 if __name__ == '__main__':
